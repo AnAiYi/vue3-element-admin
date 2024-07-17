620db83d498deaab345563eb38b7395d751635f7
wangzhihui <2878459042@qq.com>
2024-07-17 14:33:54 +0800

    feat: vite.config.ts - 自动导入配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-// import path from 'node:path';
+
 import path from 'path';
+
+import AutoImport from 'unplugin-auto-import/vite'; 
+import Components from 'unplugin-vue-components/vite';
 // https://vitejs.dev/config/
 const pathSrc = path.resolve(__dirname, 'src');
 console.log('pathSrc',pathSrc)
@@ -12,6 +15,18 @@ export default defineConfig({
       '@': pathSrc,
     },
   },
-  plugins: [vue()],
- 
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: ['vue', 'vue-router'],
+      dts: path.resolve(pathSrc, 'auto-imports.d.ts'), // 指定自动导入函数类型声明文件路径
+      eslintrc: {
+        enabled: true, // 是否自动生成 eslint 规则，建议生成之后设置 false 
+        filepath: "./.eslintrc-auto-import.json", // 指定自动导入函数 eslint 规则的文件
+      },
+    }),
+    Components({
+      dts: path.resolve(pathSrc, 'components.d.ts'), // 指定组件类型声明文件路径
+    })
+  ],
 })
