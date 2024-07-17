d3e4ebcaf4a2d544da30a232594a56b64f119906
wangzhihui <2878459042@qq.com>
2024-07-17 11:33:11 +0800

    feat: 路径别名配置，@代替src

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+// import path from 'node:path';
+import path from 'path';
 // https://vitejs.dev/config/
+const pathSrc = path.resolve(__dirname, 'src');
+console.log('pathSrc',pathSrc)
+
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': pathSrc,
+    },
+  },
   plugins: [vue()],
+ 
 })
