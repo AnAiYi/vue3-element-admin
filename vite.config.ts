5cd0d2ebeda4609c58344a7740402b2e7bfac2d3
wangzhihui <2878459042@qq.com>
2024-07-18 18:17:42 +0800

    feat: 本地开发反向代理

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { ConfigEnv, UserConfig, defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 import path from "path";
@@ -11,53 +11,71 @@ import IconsResolver from "unplugin-icons/resolver";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
-import UnoCSS from 'unocss/vite';
+import UnoCSS from "unocss/vite";
 // https://vitejs.dev/config/
 
 const pathSrc = path.resolve(__dirname, "src");
-console.log("pathSrc", pathSrc);
-// console.log('import.meta.env.VITE_APP_BASE_URL',import.meta.env.VITE_APP_BASE_URL)
-export default defineConfig({
-  resolve: {
-    alias: {
-      "@": pathSrc,
-    },
-  },
-  plugins: [
-    vue(),
-    AutoImport({
-      imports: ["vue", "vue-router"],
-      dts: path.resolve(pathSrc, "auto-imports.d.ts"), // 指定自动导入函数类型声明文件路径
-      eslintrc: {
-        enabled: true, // 是否自动生成 eslint 规则，建议生成之后设置 false
-        filepath: "./.eslintrc-auto-import.json", // 指定自动导入函数 eslint 规则的文件
-      },
-      resolvers: [ElementPlusResolver(), IconsResolver({})],
-      vueTemplate: true,
-    }),
-    Components({
-      dts: path.resolve(pathSrc, "components.d.ts"), // 指定组件类型声明文件路径
-      resolvers: [ElementPlusResolver(), IconsResolver({})],
-    }),
-    Icons({
-      autoInstall: true,
-    }),
-    createSvgIconsPlugin({
-      // 指定需要缓存的图标文件夹
-      iconDirs: [path.resolve(process.cwd(), "src/assets/icons")],
-      // 指定symbolId格式
-      symbolId: "icon-[dir]-[name]",
-    }),
-    UnoCSS(/** options */)
-  ],
-  css: {
-    // CSS 预处理器
-    preprocessorOptions: {
-      //define global scss variable
-      scss: {
-        javascriptEnabled: true,
-        additionalData: `@use "@/styles/variables.scss" as *;`,
+export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
+  const env = loadEnv(mode, process.cwd());
+  console.log("env", env);
+  console.log("pathSrc", pathSrc);
+  return {
+    resolve: {
+      alias: {
+        "@": pathSrc,
       },
     },
-  },
+    plugins: [
+      vue(),
+      AutoImport({
+        imports: ["vue", "vue-router"],
+        dts: path.resolve(pathSrc, "auto-imports.d.ts"), // 指定自动导入函数类型声明文件路径
+        eslintrc: {
+          enabled: true, // 是否自动生成 eslint 规则，建议生成之后设置 false
+          filepath: "./.eslintrc-auto-import.json", // 指定自动导入函数 eslint 规则的文件
+        },
+        resolvers: [ElementPlusResolver(), IconsResolver({})],
+        vueTemplate: true,
+      }),
+      Components({
+        dts: path.resolve(pathSrc, "components.d.ts"), // 指定组件类型声明文件路径
+        resolvers: [ElementPlusResolver(), IconsResolver({})],
+      }),
+      Icons({
+        autoInstall: true,
+      }),
+      createSvgIconsPlugin({
+        // 指定需要缓存的图标文件夹
+        iconDirs: [path.resolve(process.cwd(), "src/assets/icons")],
+        // 指定symbolId格式
+        symbolId: "icon-[dir]-[name]",
+      }),
+      UnoCSS(/** options */),
+    ],
+    css: {
+      // CSS 预处理器
+      preprocessorOptions: {
+        //define global scss variable
+        scss: {
+          javascriptEnabled: true,
+          additionalData: `@use "@/styles/variables.scss" as *;`,
+        },
+      },
+    },
+    server: {
+      host: "0.0.0.0",
+      port: Number(env.VITE_APP_PORT),
+      open: true, // 运行是否自动打开浏览器
+      // 反向代理
+      proxy: {
+        [env.VITE_APP_BASE_API]: {
+          target: "http://vapi.youlai.tech",
+          changeOrigin: true,
+          // eg: http://localhost:3000/dev-api/users/me => http://vapi.youlai.tech/users/me
+          rewrite: (path) =>
+            path.replace(new RegExp(`^${env.VITE_APP_BASE_API}`), ""),
+        },
+      },
+    },
+  };
 });
